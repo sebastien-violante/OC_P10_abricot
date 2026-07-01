3598bfd1a9ef9f77d0cdf57b3012c71b5900d2e6
sebastien-violante <sebastien.violante@gmail.com>
2026-07-01 16:47:59 +0200

    mise en place récupération token

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr" className={`${inter.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">
+      <body>
         {children}
       </body>
     </html>
