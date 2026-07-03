17dcf8dc39c49c0bd1b030e0c73025f13a976143
sebastien-violante <sebastien.violante@gmail.com>
2026-07-03 11:57:51 +0200

    inscription ok

--- src/app/utils/validateForm.ts
+++ src/app/utils/validateForm.ts
@@ -9,13 +9,27 @@ import type { FormData, FormErrors } from "@/types/types"
 export default function validateForm(formData: FormData): FormErrors {
     
     const errors: FormErrors = {}
+
+    const validateName = (name: string) => {
+        return /^[a-zA-Z0-9]+$/.test(name)
+    }
+
     if(!formData.email.trim()) {
         errors.email = "Le champ email est requis"
     } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/.test(formData.email)) {
         errors.email = "Le format du mail est invalide"
     }
-    if(formData.password.length < 8) {
+    if(formData.password.trim().length < 8) {
         errors.password = "Le mot de passe doit contenir au moins 8 caractères"
+    }
+    if(formData.type) {
+        console.log(formData.type)
+        if(!formData.name || formData.name.trim().length < 2) {
+            errors.name = "Le pseudo doit avoir au moins 2 caractères"
+        } else if (!validateName(formData.name)) {
+            console.log(validateName(formData.name))
+            errors.name = "Le pseudo ne doit comprendre que des caractères alphanumériques"
+        }
     }
 
     return errors
